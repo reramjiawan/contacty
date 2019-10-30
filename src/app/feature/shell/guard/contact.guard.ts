71892de8807904a4d24d81438e8949d2f704b5db
Ravindre Ramjiawan <ravindreramjiawan@hotmail.com>
2019-10-30 22:16:16 +0100

    revert old guard

--- src/app/feature/shell/guard/contact.guard.ts
+++ src/app/feature/shell/guard/contact.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate } from '@angular/router';
-import { ContactEntity } from '../../contact/state/contact.entity';
 import { ContactQuery } from '../../contact/state/contact.query';
 import { ContactService } from '../../contact/state/contact.service';
 
@@ -8,19 +7,13 @@ import { ContactService } from '../../contact/state/contact.service';
   providedIn: 'root'
 })
 export class ContactGuard implements CanActivate {
-  private previous: ContactEntity;
-
   constructor(private contact: ContactService,
               private contactQuery: ContactQuery) {
   }
 
   canActivate(next: ActivatedRouteSnapshot) {
     const can = this.contactQuery.getEntity(next.params.id);
-    if (!this.previous) {
-      this.previous = can;
-      this.contact.setActive(next.params.id);
-    }
-    if (can.id !== this.previous.id) {
+    if (!!can) {
       this.contact.setActive(next.params.id);
     }
     return !!can;
